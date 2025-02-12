e0c5ad22aa4d1cc901f6acbe11d1bc072d878aad
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2025-02-12 20:44:47 +0400

    feat: добавлен первый хендлер

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ from telebot import TeleBot
 from settings import TOKEN
 
 bot = TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def greet(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет! Это бот показывания погоды, пока что находится в разработке.'
+    )
+
+
 bot.polling()
